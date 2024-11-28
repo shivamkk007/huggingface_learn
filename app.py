c2cee383451401e76d1ce0d5f0dd728fb44a5a0c
Yash <21bcs5128@cuchd.in>
2024-11-28 22:22:05 +0530

    code complete and working

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
-import gradio
+import gradio as gr
 import requests
 import io
 from PIL import Image
 import requests
 
+#access tocken for the model
 API_URL = "https://api-inference.huggingface.co/models/sd-community/sdxl-flash"
-headers = {"Authorization": "Bearer hf_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"}
+headers = {"Authorization": ""}
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.content
-image_bytes = query({
-	"inputs": "Astronaut riding a horse",
-})
 
-# You can access the image with PIL.Image for example
-import io
-from PIL import Image
-image = Image.open(io.BytesIO(image_bytes))
+# function to generate image
+def generate_picture(prompt):
+	image_bytes = query({"inputs": prompt})
+	image = Image.open(io.BytesIO(image_bytes))
+	return image
+
+# Gradio interface
+trial =gr.Interface(fn=generate_picture,inputs="text", outputs="image")
+
+trial.launch(share=True)
